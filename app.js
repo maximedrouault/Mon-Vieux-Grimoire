5c3591fb97d2dc38017ce1a0c866f3c66f56d0d2
maximedrouault <maximedrouault@gmail.com>
2023-05-01 15:00:04 +0200

    Intégration de MongoDB / Routes de bases CRUD / Model Mongoose.

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const Book = require("./models/Book");
+
+mongoose.connect("mongodb+srv://maximedrouault:9h1nEnZbeMGT42IS@cluster0.fnbnars.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 const app = express();
 
@@ -11,33 +20,40 @@ app.use((req, res, next) => {
 	next();
   });
 
-app.post('/api/stuff', (req, res, next) => {
-	console.log(req.body);
-	res.status(201).json({
-		message: 'Objet créé !'
+app.post("/api/books", (req, res, next) => {
+	// à confirmer en fonction du Body du Front
+	delete req.body._id;
+	// 
+	const book = new Book({
+		...req.body
 	});
+	book.save()
+		.then(() => res.status(201).json({ message: "Objet enregistré !" }))
+		.catch(error => res.status(400).json({ error }));
 });
 
-app.get("/api/stuff", (req, res, next) => {
-	const stuff = [
-	  {
-		_id: "oeihfzeoi",
-		title: "Mon premier objet",
-		description: "Les infos de mon premier objet",
-		imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-		price: 4900,
-		userId: "qsomihvqios",
-	  },
-	  {
-		_id: "oeihfzeomoihi",
-		title: "Mon deuxième objet",
-		description: "Les infos de mon deuxième objet",
-		imageUrl: "https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg",
-		price: 2900,
-		userId: "qsomihvqios",
-	  },
-	];
-	res.status(200).json(stuff);
-  });
+app.put("/api/books/:id", (req, res, next) => {
+	Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Objet modifié !" }))
+		.catch(error => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+	Book.deleteOne({ _id: req.params.id })
+		.then(() => res.status(200).json({ message: "Objet supprimé !" }))
+		.catch(error => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+	Book.findOne({ _id: req.params.id })
+		.then(book => res.status(200).json(book))
+		.catch(error => res.status(404).json({ error }));
+});
+
+app.get("/api/books", (req, res, next) => {
+	Book.find()
+		.then(books => res.status(200).json(books))
+		.catch(error => res.status(400).json({ error }));
+});
 
 module.exports = app;
